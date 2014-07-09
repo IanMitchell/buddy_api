ee177f165420dbe3267fabb2a83b970affc2fe95
Ian Mitchell <ian.mitchel1@live.com>
2014-07-09 11:51:50 -0700

    Added Videos

--- lib/buddy_api/video.rb
+++ lib/buddy_api/video.rb
@@ -1,31 +1,138 @@
 module BuddyAPI
   module Video
-    # Public: TODO: Implement
-    def create
+    CREATE_PATH   = '/videos'
+    GET_PATH      = '/videos' # + id
+    GET_FILE_PATH = '/videos/#{id}/file'
+    SEARCH_PATH   = '/videos'
+    UPDATE_PATH   = '/videos' # + id
+    DELETE_PATH   = '/videos' # + id
+
+    # Public: TODO: Test, Document
+    def self.create(token, data, options)
+      params = { data: data }
+      params.merge! options
+
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        CREATE_PATH,
+                                        options: params,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def get
+    # Public: TODO: Test, Document
+    def self.get(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        GET_PATH + "/#{id}",
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement, Research
-    def get
+    # Public: TODO: Test, Document
+    def self.get_file(token, id)
+      path = GET_FILE_PATH.gsub '#{id}', id.to_s
+
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        path,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement, Research
-    def get_file
+    # Public: TODO: Test, Document
+    def self.search(token, options)
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        SEARCH_PATH,
+                                        options: options,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def search
+    # Public: TODO: Test, Document
+    def self.update(token, id, options)
+      response = BuddyAPI.buddy_request(BuddyAPI::PATCH,
+                                        UPDATE_PATH + "/#{id}",
+                                        token: token,
+                                        options: options)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def update
-    end
+    # Public: TODO: Test, Document
+    def self.delete(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::DELETE,
+                                        DELETE_PATH + "/#{id}",
+                                        token: token)
 
-    # Public: TODO: Implement
-    def delete
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
   end
 end
