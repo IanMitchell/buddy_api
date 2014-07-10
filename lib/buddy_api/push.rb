5e56d2f81dd222282f5766315602edec717f2c49
Ian Mitchell <ian.mitchel1@live.com>
2014-07-10 11:49:26 -0700

    Added Push

--- lib/buddy_api/push.rb
+++ lib/buddy_api/push.rb
@@ -1,11 +1,49 @@
 module BuddyAPI
   module Push
-    # Public: TODO: Implement
-    def send
+    SEND_PATH           = '/notifications'
+    RECORD_RECIEVE_PATH = '/notifications/received' # + id
+
+    # Public: TODO: Test, Document
+    def self.send(token, options)
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        SEND_PATH,
+                                        options: options,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def record_recieve
+    # Public: TODO: Test, Document
+    def self.record_recieve(token, id)
+      path = RECORD_RECIEVE_PATH + "/#{id}"
+
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        path,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
   end
 end
