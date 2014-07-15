a6107b7fc9159621227b2eca777d54b57c433033
Ian Mitchell <ian.mitchel1@live.com>
2014-07-15 10:51:37 -0700

    Added Album
    Currently a documentation error for Album.search, so that method is
    unimplemented at the moment.

--- lib/buddy_api/album.rb
+++ lib/buddy_api/album.rb
@@ -1,23 +1,98 @@
 module BuddyAPI
   module Album
-    # Public: TODO: Implement
-    def create
+    CREATE_PATH = '/albums'
+    GET_PATH    = '/albums' # + id
+    SEARCH_PATH = ''
+    UPDATE_PATH = '/albums' # + id
+    DELETE_PATH = '/albums' # + id
+
+    # Public: TODO: Test, Document
+    def self.create(token, name, options = {})
+      params = { name: name }
+      params.merge! options
+
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        CREATE_PATH,
+                                        options: params,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def get
+    # Public: TODO: Test, Document
+    def self.get(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        GET_PATH + "/#{id}",
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
     # Public: TODO: Implement
     def search
     end
 
-    # Public: TODO: Implement
-    def update
+    # Public: TODO: Test, Document
+    def self.update(token, id, options = {})
+      response = BuddyAPI.buddy_request(BuddyAPI::PATCH,
+                                        UPDATE_PATH + "/#{id}",
+                                        token: token,
+                                        options: options)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def delete
+    # Public: TODO: Test, Document
+    def self.delete(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::DELETE,
+                                        DELETE_PATH + "/#{id}",
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
   end
 end
