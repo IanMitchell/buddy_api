7f53f8671dd1e26540b0085caaf300bfcd204af0
Ian Mitchell <ian.mitchel1@live.com>
2014-07-15 11:14:11 -0700

    Added Messages

--- lib/buddy_api/message.rb
+++ lib/buddy_api/message.rb
@@ -1,23 +1,120 @@
 module BuddyAPI
   module Message
-    # Public: TODO: Implement
-    def send
+    SEND_PATH   = '/messages'
+    GET_PATH    = '/messages' # + id
+    UPDATE_PATH = '/messages' # + id
+    SEARCH_PATH = '/messages'
+    DELETE_PATH = '/messages' # + id
+
+    # Public: TODO: Test, Document
+    def self.send(token, addressees, subject, options = {})
+      params = { addressees: addressees, subject: subject }
+      params.merge! options
+
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        SEND_PATH,
+                                        options: params,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def get
+    # Public: TODO: Test, Document
+    def self.get(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        GET_PATH + "/#{id}",
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def update
+    # Public: TODO: Test, Document
+    def self.update(token, id, is_new)
+      params = { isNew: is_new }
+
+      response = BuddyAPI.buddy_request(BuddyAPI::PATCH,
+                                        UPDATE_PATH + "/#{id}",
+                                        token: token,
+                                        options: params)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def search
+    # Public: TODO: Test, Document
+    def self.search(token, location_range, options = {})
+      params = { locationRange: location_range }
+      params.merge! options
+
+      response = BuddyAPI.buddy_request(BuddyAPI::GET,
+                                        SEARCH_PATH,
+                                        options: params,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
 
-    # Public: TODO: Implement
-    def delete
+    # Public: TODO: Test, Document
+    def self.delete(token, id)
+      response = BuddyAPI.buddy_request(BuddyAPI::DELETE,
+                                        DELETE_PATH + "/#{id}",
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return true
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
   end
 end
