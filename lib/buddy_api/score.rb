3cf49668f4731d15bd4a32c50ea03e84a42315fb
Ian Mitchell <ian.mitchel1@live.com>
2014-07-15 10:36:22 -0700

    Added Score

--- lib/buddy_api/score.rb
+++ lib/buddy_api/score.rb
@@ -1,7 +1,30 @@
 module BuddyAPI
   module Score
-    # Public: TODO: Implement
-    def create
+    CREATE_PATH = '/games/#{gameId}/sessions/#{sessionId}/scores'
+
+    # Public: TODO: Test, Document
+    def create(token, game_id, session_id, score = nil, options)
+      path = CREATE_PATH.gsub('#{gameId}', game_id.to_s)
+                        .gsub('#{sessionId}', session_id.to_s)
+      path += "?score=#{score}" unless score.nil?
+
+      response = BuddyAPI.buddy_request(BuddyAPI::POST,
+                                        path,
+                                        options: options,
+                                        token: token)
+
+      body = JSON.parse(response.body)
+
+      case response.code
+      when '401', '400'
+        BuddyAPI.buddy_error(body)
+      when '200'
+        return body
+      else
+        raise UnknownResponseCode, BuddyAPI.error_message(self,
+                                                          __method__,
+                                                          response.code)
+      end
     end
   end
 end
