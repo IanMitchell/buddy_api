899077373b33050173bc65753174586af563b28c
Ian Mitchell <ian.mitchel1@live.com>
2014-07-16 11:25:21 -0700

    Score Test Skeleton

---
+++ test/score_test.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class ScoreTest < Test::Unit::TestCase
+  def test_create_success
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_create_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_create_nonexistant
+    assert false, "Unimplemented"
+  end
+end
