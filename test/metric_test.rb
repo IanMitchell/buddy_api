cbc6b687db8aaf0a2ec563e3e15c2b558f9baf64
Ian Mitchell <ian.mitchel1@live.com>
2014-07-16 11:22:53 -0700

    Metric Test Skeleton

---
+++ test/metric_test.rb
@@ -0,0 +1,45 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class MetricTest < Test::Unit::TestCase
+  def test_record_success
+    assert false, "Unimplemented"
+  end
+
+  def test_record_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_record_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_record_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_success
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_finish_disabled
+    assert false, "Unimplemented"
+  end
+end
