c67a3dce10553566456b754fd9a32b159bf0f7bc
Ian Mitchell <ian.mitchel1@live.com>
2014-07-17 09:54:09 -0700

    Player Test Skeleton

---
+++ test/player_test.rb
@@ -0,0 +1,113 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class PlayerTest < Test::Unit::TestCase
+  def test_create_success
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_create_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_success
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_nonexistant
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_get_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_search_success
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_update_success
+    assert false, "Unimplemented"
+  end
+
+  def test_update_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_update_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_update_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_update_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_update_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_success
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_authorization
+    assert false, "Unimplemented"
+  end
+end
