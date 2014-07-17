a09a501dd09bd0a10da5eccc230409e76642ad12
Ian Mitchell <ian.mitchel1@live.com>
2014-07-17 10:24:38 -0700

    User List Test Skeleton

---
+++ test/user_list_test.rb
@@ -0,0 +1,97 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class UserListTest < Test::Unit::TestCase
+  def test_create_success
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_create_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_success
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_add_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_add_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_add_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_add_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_add_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_success
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_bad_authorization
+    assert false, "Unimplemented"
+  end
+  
+  def test_get_success
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_missing_parameter
+    assert false, "Unimplemented"
+  end
+end
