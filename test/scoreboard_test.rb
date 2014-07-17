db1b6982ae08c27a5ab73f1383a8b0871dea20b1
Ian Mitchell <ian.mitchel1@live.com>
2014-07-17 10:16:37 -0700

    Scoreboard Test Skeleton

---
+++ test/scoreboard_test.rb
@@ -0,0 +1,81 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class ScoreboardTest < Test::Unit::TestCase
+  def test_create_success
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_create_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_create_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_success
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_search_success
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_search_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_success
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_missing_parameter
+    assert false, "Unimplemented"
+  end
+end
