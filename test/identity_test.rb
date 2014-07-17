63dbd1ba5dc90601418f5bad3ef0020cc4473d8a
Ian Mitchell <ian.mitchel1@live.com>
2014-07-17 08:56:22 -0700

    Identity Test Skeleton

---
+++ test/identity_test.rb
@@ -0,0 +1,85 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class IdentityTest < Test::Unit::TestCase
+  def test_add_success
+    assert false, "Unimplemented"
+  end
+
+  def test_add_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_add_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_add_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_add_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_success
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_remove_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_find_success
+    assert false, "Unimplemented"
+  end
+
+  def test_find_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_find_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_find_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_find_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_get_success
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_disabled
+    assert false, "Unimplemented"
+  end
+end
