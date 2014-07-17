1f17f25445b3b403ff6a259e8a1b4ea61a5c6dc5
Ian Mitchell <ian.mitchel1@live.com>
2014-07-17 09:11:09 -0700

    Metadata Test Skeleton

---
+++ test/metadata_test.rb
@@ -0,0 +1,142 @@
+require 'test/unit'
+require 'test_helper'
+require 'buddy_api'
+
+class MetadataTest < Test::Unit::TestCase
+  def test_set_success
+    assert false, "Unimplemented"
+  end
+
+  def test_set_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_set_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_set_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_set_nonexistant
+    assert false, "Unimplemented"
+  end
+
+  def test_set_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_set_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_success
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_nonexistant
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_increment_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_get_success
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_get_nonexistant
+    assert false, "Unimplemented"
+  end
+
+  def test_get_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_get_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_search_success
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_search_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_search_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_search_disabled
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_success
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_token
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_missing_parameter
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_nonexistent
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_bad_authorization
+    assert false, "Unimplemented"
+  end
+
+  def test_delete_disabled
+    assert false, "Unimplemented"
+  end
+
+end
